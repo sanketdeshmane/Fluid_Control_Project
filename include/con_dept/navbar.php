af49ee8a938af5471c994ba0d9c37e7c63b25f41
Shrinidhi1904 <63735198+Shrinidhi1904@users.noreply.github.com>
2021-10-23 14:32:14 +0530

    Add files via upload

---
+++ include/con_dept/navbar.php
@@ -0,0 +1,59 @@
+
+<body>
+	<nav class="sb-topnav navbar navbar-expand navbar-light sticky-top" style="padding-left: 0; padding-top: 0; padding-bottom: 0; z-index: 1040;">
+		<a class="navbar-brand" href="" style="padding: 0;"><img class="logo" src="../assets/images.png" alt="fluid_control" width="auto" height="56px"></a> <a class="logout-button pl-3" id="sidebarToggle"><i class="fas fa-bars"style="color:black"></i></a>
+		<ul class="navbar-nav ml-auto ">
+			<li class="nav-item">
+					<form>
+						<a class="logout-button" type="button" name="logout-btn" href=""><i class="fas fa-power-off fa-2x"></i></a>
+					</form>
+			</li>
+		</ul>
+	</nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="profile_info pb-">
+                            <i class="fas fa-user-circle fa-3x pb-"style="color:white"></i>
+                            <h4 class="profile-info-text pt-2"><?php echo $emp_name?></h4>
+                            <h4 class="profile-info-text pb-3" style="font-size: small;">Concerned Department</h4>
+							<small class ="profile-info-text pb-3"><a href="" class="edit_profile">Edit Profile</a></small>
+                        </div><br>
+                        <a class="pt-3 nav-link" href="con_index.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-user"></i></div>
+                            Dashboard<br>
+                        </a>
+                        <!-- <a class="pt-3 nav-link" href="solution.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-bug"></i></div>
+                            Defect Solution <br>
+                        </a> -->
+                       
+                        <a class="nav-link" href="con_notification.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-comment"></i></div>
+                            Notifications
+                        </a>
+						
+						<a class="pt-3 nav-link" href="disapproved.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-exclamation"></i></div>
+                            Disapproved Solutions <br>
+                        </a>
+						
+                        <a class="nav-link" href="">
+                            <div class="sb-nav-link-icon"><i class="fa fa-file"></i></div>
+                            Analysis Report
+                        </a>
+						<a class="pt-3 nav-link" href="">
+                            <div class="sb-nav-link-icon"><i class="fas fa-file-alt"></i></div>
+                            Preventive Measures<br>
+                        </a>
+                    </div>
+                </div>
+                <div class="small">
+                    <div class="text-muted p-2 pb-2">Copyright &copy; Fluid Control 2021</div>
+                </div>
+            </nav>
+        </div>
+        
+        <div id="layoutSidenav_content">
